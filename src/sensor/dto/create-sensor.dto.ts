036982a411af0cd32431e9906ee3b653e69d7d26
Juan Rodriguez <juanrogue@gmail.com>
2025-09-18 08:53:04 +0200

    Created class CreateSensorDto

--- src/sensor/dto/create-sensor.dto.ts
+++ src/sensor/dto/create-sensor.dto.ts
@@ -1,1 +1,8 @@
-export class CreateSensorDto {}
+export class CreateSensorDto {
+    uuid: string;
+    sensorId: string; // Internal id from device if avalaibe    
+    userId: string;// From keycloak instance
+    metadata: string;
+    updatedAt: string;
+    createdAt: string;
+}
